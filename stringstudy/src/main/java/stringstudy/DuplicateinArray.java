64a293c8a63c71c2d5ada1319bea68c4e3773ffa
mytade1086 <mytrade1086@gmail.com>
2019-03-30 19:05:18 +0530

    Hashset logic for duplicate in array

--- stringstudy/src/main/java/stringstudy/DuplicateinArray.java
+++ stringstudy/src/main/java/stringstudy/DuplicateinArray.java
@@ -1,11 +1,15 @@
 package stringstudy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DuplicateinArray {
 
 	public static void main(String[] args) {
 
-		String[] lang = { "java", ".net", "csharp", "c","java","c" };
+		String[] lang = { "java", ".net", "csharp", "c", "java", "c" };
 
+		// 1. Solution #Worst as Time Complexity is
 		for (int i = 0; i < lang.length; i++) {
 
 			for (int j = i + 1; j < lang.length; j++) {
@@ -15,6 +19,19 @@ public class DuplicateinArray {
 					System.out.println(lang[i]);
 
 				}
+			}
+		}
+
+		// 2. Using HashSet. Add method returns false if similar data already entered
+
+		Set<String> lang2 = new HashSet<String>();
+
+		for (String prog : lang) {
+
+			if (lang2.add(prog) == false) {
+
+				System.out.println("Duplicate found for " + prog);
+
 			}
 		}
 
