3ac876e12debbbbb54db6453a748e8dfc31293ad
mytade1086 <mytrade1086@gmail.com>
2019-04-03 01:33:47 +0530

    Armstrong and A to Z printing

--- javapgms/src/main/java/com/javapgms/Armstrong.java
+++ javapgms/src/main/java/com/javapgms/Armstrong.java
@@ -1,5 +1,27 @@
 package com.javapgms;
 
 public class Armstrong {
+	
+	
+	// 153 = 1*1*1 + 5*5*5 + 3*3*3  // 153 is an Armstrong number.
+	
+	 public static void main(String[] args) {
+
+	        int number = 371, originalNumber, remainder, result = 0;
+
+	        originalNumber = number;
+
+	        while (originalNumber != 0)
+	        {
+	            remainder = originalNumber % 10;
+	            result += Math.pow(remainder, 3);
+	            originalNumber /= 10;
+	        }
+
+	        if(result == number)
+	            System.out.println(number + " is an Armstrong number.");
+	        else
+	            System.out.println(number + " is not an Armstrong number.");
+	    }
 
 }
---
+++ javapgms/src/main/java/com/javapgms/PrintAtoZ.java
@@ -0,0 +1,14 @@
+package com.javapgms;
+
+	public class PrintAtoZ {
+
+	    public static void main(String[] args) {
+
+	        char c;
+
+	        for(c = 'A'; c <= 'Z'; ++c)
+	            System.out.print(c + " ");
+	    }
+	}
+
+
