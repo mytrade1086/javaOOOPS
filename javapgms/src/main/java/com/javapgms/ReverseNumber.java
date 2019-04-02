584b7fff23b7fa16d7787124d3db0ba858839977
mytade1086 <mytrade1086@gmail.com>
2019-04-03 01:20:57 +0530

    Palindrome logic

--- javapgms/src/main/java/com/javapgms/ReverseNumber.java
+++ javapgms/src/main/java/com/javapgms/ReverseNumber.java
@@ -1,32 +1,26 @@
 package com.javapgms;
 
 public class ReverseNumber {
-	
-	
+
 	public static void main(String[] args) {
-		
-		
-		long num=12345;
-		
-		long rev=0;
-		
-		while(num!=0){
-			
-			rev=rev*10+num%10;
-			num=num/10;
-			
-			
+
+		long num = 12345;
+
+		long rev = 0;
+
+		while (num != 0) {
+
+			rev = rev * 10 + num % 10;
+			num = num / 10;
+
 		}
-		
+
 		System.out.println(rev);
-		
-		
-		
-		
-		//2 Soltion 2:
-		
-		long num2=4545;
-		StringBuffer sb=new StringBuffer(String.valueOf(num2)).reverse();
+
+		// 2 Solution 2:
+
+		long num2 = 4545;
+		StringBuffer sb = new StringBuffer(String.valueOf(num2)).reverse();
 		System.out.println(sb);
 	}
 
