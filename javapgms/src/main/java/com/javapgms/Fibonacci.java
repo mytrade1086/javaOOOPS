1460b39893b841d9b1bc784353d970fd30f43600
mytade1086 <mytrade1086@gmail.com>
2019-04-02 21:00:03 +0530

    Fibonacci pgm with for loop

--- javapgms/src/main/java/com/javapgms/Fibonacci.java
+++ javapgms/src/main/java/com/javapgms/Fibonacci.java
@@ -1,5 +1,25 @@
 package com.javapgms;
 
+/*The Fibonacci series is a series where the next term is the sum of pervious two terms. 
+The first two terms of the Fibonacci sequence is 0 followed by 1.
+
+The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, ..
+*/
+
 public class Fibonacci {
+
+	public static void main(String[] args) {
+
+		int n = 10, t1 = 0, t2 = 1;
+		System.out.println("First " + n + " terms: ");
+
+		for (int i = 1; i <= n; ++i) {
+			System.out.print(t1 + " , ");
+
+			int sum = t1 + t2;
+			t1 = t2;
+			t2 = sum;
+		}
+	}
 
 }
